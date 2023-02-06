e7d417ae381c92af6343a7297ee64d82c90c766f
Al <almizan1040@gmail.com>
2023-02-06 17:45:13 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+function myFunction() {
+    event.preventDefault()
+    const email = document.getElementById("email").value
+    const sub = document.getElementById("sub").value
+    const mess = document.getElementById("mess").value
+    // const data = { "Email": email, "sub": sub, "Message": mess }
+    const data = { email, sub, mess }
+    // console.log(data);
+    fetch('http://localhost:3000/', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if(data === true){
+                alert('Message sent successfully')
+            }
+            else{
+                alert('Unsent')
+            }
+            // console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
